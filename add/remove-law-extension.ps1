e8a4dcce4e4ceabcf7d0655bfc6913c72db0b0c2
Martin Fahlbeck <77161991+marfha88@users.noreply.github.com>
2022-04-11 14:04:25 +0200

    changed name and scripts

--- add/remove-law-extension.ps1
+++ add/remove-law-extension.ps1
@@ -1,10 +1,10 @@
-Set-AzContext -Subscription "0563c2c3-2a39-4c80-bbaa-93196e049cd2"
-$resourcegroupname = "HUB-PROD-MSS-RESOURCES"
-$Location = "westeurope"
-$name = "ms"
+Set-AzContext -Subscription "your subscription"
+$resourcegroupname = "your RG"
+$Location = "your location"
+$name = "exampel of name"
 
-$workspaceId = "903fd8e2-d3bf-4bea-8dd2-566f56a71881"
-$workspaceKey = "/pTTBF2nnBuRojEYmN7bf/JAy3LM0RDjxZEDoQRUf2r8PsOKcymsoVkXtFrIrOJlMn8wn3etaKvJifkBJqMTXw=="
+$workspaceId = "your LAW id"
+$workspaceKey = "your LAW key"
                                                              
 $Publicsettings=@{"workspaceId" = $workspaceId}
  
@@ -17,11 +17,11 @@ $vms.Name
 foreach($vm in $vms){
 $vmName=$vm.name
 
-
 Remove-AzVMExtension -ResourceGroupName $resourceGroupName -VMName $vmName -Name MicrosoftMonitoringAgent -Force
 
 }
-<#
+
+{
 Set-AzVMExtension -ExtensionName "AzureMonitorWindowsAgent" `
                   -ResourceGroupName $resourceGroupName `
                   -VMName $vmName `
@@ -33,5 +33,5 @@ Set-AzVMExtension -ExtensionName "AzureMonitorWindowsAgent" `
                   -Location $Location `
                   -EnableAutomaticUpgrade $true
 }
-#>
+
 
